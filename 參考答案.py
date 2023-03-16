4fb923423756425e08e27c8cd1dbb509ff16b9b3
toukinxi <52906964+Prometheustou@users.noreply.github.com>
2023-03-16 15:17:23 +0800

    Update 參考答案.py

--- 參考答案.py
+++ 參考答案.py
@@ -15,12 +15,16 @@ if __name__ == '__main__':
     tl_camera.set_fps("high")
     tl_camera.set_resolution("high")
     tl_camera.set_bitrate(6)
+    tl_flight.forward(distance=50).wait_for_completed()
     for i in range(0, 302):
+        tl_flight.rotate(angle=i+1).wait_for_completed()
         img = tl_camera.read_cv2_image()
         cv2.imshow("Drone", img)
+        file_name = "C:\\Users\\Teacher\\Desktop\\save\\image" + "_" + str(i+1) + '.jpg'
+        cv2.imwrite(file_name, img)  # 以上面的檔案名，儲存切割的圖片
         cv2.waitKey(1)
-    tl_flight.forward(distance=50).wait_for_completed()
-    tl_flight.rotate(angle=360).wait_for_completed()
+    
+    
     cv2.destroyAllWindows()
     tl_camera.stop_video_stream()
 
