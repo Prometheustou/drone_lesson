dee0320bb4ffb0f1a35a8c2dd10504c1f0d2c5ad
toukinxi <52906964+Prometheustou@users.noreply.github.com>
2023-03-19 12:02:03 +0800

    Update 參考答案.py

--- 參考答案.py
+++ 參考答案.py
@@ -4,42 +4,52 @@ import threading
 from time import sleep
 
 
-def take_photo():
-    tl_camera = tl_drone.camera
-    # 显示302帧图传
-    tl_camera.start_video_stream(display=False)
-    tl_camera.set_fps("high")
-    tl_camera.set_resolution("high")
-    tl_camera.set_bitrate(6)
-    for i in range(0, 302):
-        tl_battery = tl_drone.battery
-        battery_info = tl_battery.get_battery()
-        print("Drone battery soc: {0}".format(battery_info))
-        img = tl_camera.read_cv2_image()
-        cv2.imshow("Drone", img)
-        file_name = "C:\\Users\\acer\\Desktop\\save\\image_" + str(i + 1) + ".jpg"
-        cv2.imwrite(file_name, img)
-        cv2.waitKey(1)
-    cv2.destroyAllWindows()
-    tl_camera.stop_video_stream()
-    tl_drone.close()
+class MyThread1(threading.Thread):
+    def run(self):
+        tl_camera = tl_drone.camera
+        # 显示302帧图传
+        tl_camera.start_video_stream(display=False)
+        tl_camera.set_fps("high")
+        tl_camera.set_resolution("high")
+        tl_camera.set_bitrate(6)
+        k=1
+        for i in range(0, 2300):
+            img = tl_camera.read_cv2_image()
+            cv2.imshow("Drone", img)
+            if i % 10 == 0:
+                file_name = "C:\\Users\\Teacher\\Desktop\\save\\image_" + str(k) + ".jpg"
+                cv2.imwrite(file_name, img)
+                k = k+1
+            cv2.waitKey(1)
+        cv2.destroyAllWindows()
+        tl_camera.stop_video_stream()
+        tl_drone.close()
 
 
-def flight_fun():
-    for i in range(36):
-        tl_flight.rotate(angle=10).wait_for_completed()
-        sleep(1)
-    
+class MyThread2(threading.Thread):
+    def run(self):
+        for i in range(40):
+            tl_flight.rotate(angle=10).wait_for_completed()
+            sleep(1)
+        tl_flight.land().wait_for_completed()
+
 
 if __name__ == '__main__':
     tl_drone = robot.Drone()
     tl_drone.initialize()
     tl_flight = tl_drone.flight
+    tl_battery = tl_drone.battery.get_battery()
+    print("begin battery soc: {0}".format(tl_battery))
     tl_flight.takeoff().wait_for_completed()
-    a = threading.Thread(target=take_photo())
-    b = threading.Thread(target=flight_fun())
-    a.start()
-    b.start()
-    tl_flight.land().wait_for_completed()
+    tl_flight.forward(distance=100).wait_for_completed()
+    tl_flight.up(distance=50).wait_for_completed()
+    threadl = []
+    t1 = MyThread1()
+    t2 = MyThread2()
+    threadl.append(t1)
+    threadl.append(t2)
+    for x in threadl:
+        x.start()
+    #tl_battery = tl_drone.battery.get_battery()
+    #print("end battery soc: {0}".format(tl_battery))
     # 起飞后降落
-
